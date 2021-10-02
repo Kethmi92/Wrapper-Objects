817f3c2dbdc9259d53ffd2c910215eac80606051
Kethmi Soysa <91780060+Kethmi92@users.noreply.github.com>
2021-10-02 14:48:49 +0530

    upload the files

---
+++ Main.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        //ArrayList<int> myNum = new ArrayList<int>();
+        ArrayList<Integer> myCount = new ArrayList<Integer>();
+
+        Integer one = 5;
+        Double two = 2.00;
+        Character c = 'A';
+
+
+
+        System.out.println("one");
+        System.out.println("two");
+        System.out.println("c");
+
+//        System.out.println(one.intValue());
+//        System.out.println(two.doubleValue());
+//        System.out.println(c.charValue());
+
+        Integer myInt = 100;
+        String myString = myInt.toString();
+//        System.out.println(myString.length());
+        System.out.println(myInt);
+    }
+}
